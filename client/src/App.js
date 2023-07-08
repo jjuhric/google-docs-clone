b7acd82094376640c3c0dd5f43cd2eacc7ffcf5c
Jeff Uhrick <jjuhric@gmail.com>
2023-07-07 21:37:25 -0400

    getting basics setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+import Document from './Document'
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div>
+      <Document />
     </div>
   );
 }
---
+++ client/src/Document.js
@@ -0,0 +1,23 @@
+import { useCallback } from 'react'
+import Quill from 'quill'
+import "quill/dist/quill.snow.css"
+
+
+export default function Document() {
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return 
+
+    wrapper.innerHTML = ""
+    const editor = document.createElement('div')
+    wrapperRef.current.append(editor)
+    new Quill('#container', {
+      theme: 'snow'
+    })
+
+    return () => { wrapperRef.innerHTML = '' }
+  }, [])
+
+  return (
+    <div id="container" ref={wrapperRef}></div>
+  )
+}
