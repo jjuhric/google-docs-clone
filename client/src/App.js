1fb80f63b134ba26411ac071f6e1a829ccad6a39
Jeff Uhrick <jjuhric@gmail.com>
2023-07-08 20:52:38 -0400

    Version 1 of the google docs clone

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
-import Document from './Document'
+import TextEditor from "./TextEditor"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom"
+import { v4 as uuidV4 } from "uuid"
 
 function App() {
   return (
-    <div>
-      <Document />
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Navigate to={`/documents/${uuidV4()}`} />}>
+        </Route>
+        <Route path="/documents/:id" element={<TextEditor />}>
+        </Route>
+      </Routes>
+    </Router>
+  )
 }
 
-export default App;
+export default App
